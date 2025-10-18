ec710b60605b39cdc28898663e3b153081322ce6
Oyinuah Simeni <oyinuahsimeni@gmail.com>
2025-10-18 18:03:49 +0100

    Week 2 assignment fix

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const port = process.env.PORT;
 const requestLogger = (req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
 };    
-
+app.listen(port, () => console.log('API live on port ${port}'));
 app.use(requestLogger);
 app.use(express.json());
-
 
 app.get('/', (req, res) => {res.send('My Week 2 API!')});
 
 app.post('/user', (req, res) => {
-  const {name, email } = req.body;
+  const {name, email} = req.body;
   if (!name || !req.body.email) {
     return res.status(400).send({ error: 'Missing name or email' });
   }
@@ -24,5 +24,3 @@ app.get('/user/:id', (req, res) => {
   const userId = req.params.id;
   res.send(`User ${userId} profile`);
 });
-
-app.listen(3000, () => console.log('API live on port 3000'));
